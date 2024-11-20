ca50fbb9ea47f2a7286deb45de7bde977edd4df3
riyal-suthar <147534787+riyal-suthar@users.noreply.github.com>
2024-11-20 12:01:00 +0530

    ""

--- location_check/main.dart
+++ location_check/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:ticket_reselling/location_check/loc.dart';
 
 void main() async {
-  runApp(Check());
+  runApp(MaterialApp(home: Check()));
 }
